3ea82dab4a648f933ec21fd337d4aec682f322f4
mpcabete <mateus.cabete@usp.br>
2021-09-27 00:50:43 -0300

    vim vimplug setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 echo "source ~/myrcs/.vimrc" > ~/.vimrc
 
-
+# create persistent undo dir
 if [[ ! -d ~/.vim/undodir ]]
 then
   echo "creating vim undodir";
   mkdir -p ~/.vim/undodir
 
 fi
+
+# install vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
 xmodmap ./.Xmodmap
 cp ./.Xmodmap ~/.Xmodmap
