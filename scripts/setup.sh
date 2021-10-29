09aa71256207bd83c879d474afdf72c7b6fd2f73
mpcabete-note <mateus.cabete@usp.br>
2021-10-29 01:58:13 -0300

    zshrc, dependencies file, zsh sintax and autosugest

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,10 +14,19 @@ echo "if on ubuntu disto manually add bind-caps.sh to startup scripts"
 xmodmap -pke > ~/.Xmodmap;
 # append capslock bind
 cat ./xmodmap-capslock >> ~/.Xmodmap;
+
 # update bindings
-xmodmap ~/.Xmodmap
+# modmap ~/.Xmodmap
 
 # install vimplug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# zshrc
+echo "source ~/myrcs/.zshrc" > ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# set default as zsh
+chsh -s $(which zsh)
 
