f6f9c3b729eac8751feeaabc2b615523755e3e84
mpcabete-note <mateus.cabete@usp.br>
2021-11-05 02:16:04 -0300

    cript.sh

--- scripts/cript.sh
+++ scripts/cript.sh
@@ -1,2 +1,5 @@
-gpg --encrypt-files -a -r mateus.cabete@gmail.com *.txt
-rm *.txt
+gpg --encrypt-files -a -r mateus.cabete@gmail.com ~/journal/*.txt
+mv *.asc ~/backup/journal
+mv *.txt ~/archive
+rsync -avPh -e ssh ~/backup mp@mpcabete.xyz:/home/mp
+
